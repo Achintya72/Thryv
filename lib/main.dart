6afed74c261b51d39305d6bc45034c8df1d2154f
Pranav Kannepalli <pranav.kannepalli@gmail.com>
2024-07-20 16:50:11 -0700

    google sign in stuff

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,12 @@ import 'package:provider/provider.dart';
 import 'package:thryv/common/theme.dart';
 import 'package:thryv/firebase_options.dart';
 import 'package:go_router/go_router.dart';
+import 'package:thryv/model/user.model.dart';
 import 'package:thryv/service/auth.dart';
 import 'package:thryv/userProvider.dart';
 import 'package:thryv/views/authentication/landing.view.dart';
+import 'package:thryv/views/authentication/signin.view.dart';
+import 'package:thryv/views/authentication/signup.view.dart';
 import 'package:thryv/views/placeholder.dart';
 
 void main() async {
@@ -17,16 +20,39 @@ void main() async {
   runApp(const Thryv());
 }
 
+bool loggedIn(BuildContext context) {
+  final user = Provider.of<AuthUser?>(context, listen: false);
+  return user != null;
+}
+
+String? loggedInRedirect(BuildContext context) {
+  if (loggedIn(context)) {
+    return "/onboarding1";
+  }
+  return null;
+}
+
 final _router = GoRouter(redirectLimit: 10, initialLocation: '/landing', routes: [
   GoRoute(
     path: '/landing',
     builder: (context, state) => const LandingScreen(),
+    redirect: (context, state) => loggedInRedirect(context),
   ),
   GoRoute(
     path: '/',
     builder: (context, state) => const ThryvPlaceholder(),
     redirect: (context, state) => '/landing',
-  )
+  ),
+  GoRoute(
+    path: '/signUp',
+    builder: (context, state) => const SignUp(),
+    redirect: (context, state) => loggedInRedirect(context),
+  ),
+  GoRoute(
+    path: '/signIn',
+    builder: (context, state) => const SignIn(),
+    redirect: (context, state) => loggedInRedirect(context),
+  ),
 ]);
 
 class Thryv extends StatelessWidget {
