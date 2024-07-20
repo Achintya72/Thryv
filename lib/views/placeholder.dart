c02a2788d2b0a0925d69e774c9914aca81ef9583
PranavK226 <pranav.kannepalli@gmail.com>
2024-07-20 11:35:01 -0700

    configuring flutterfire

---
+++ lib/views/placeholder.dart
@@ -0,0 +1,10 @@
+import "package:flutter/material.dart";
+
+class ThryvPlaceholder extends StatelessWidget {
+  const ThryvPlaceholder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Hi");
+  }
+}
